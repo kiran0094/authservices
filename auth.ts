d2f6c879f1937b1d2f7e2ef02b1b687e879c9aac
Kummari Kiran <kummkiran@gmail.com>
2025-03-14 11:17:56 +0530

    production error

--- auth.ts
+++ auth.ts
@@ -31,8 +31,8 @@ declare module "next-auth/jwt" {
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
-    signIn: "auth/login",
-    error: "auth/error",
+    signIn: "/auth/login",
+    error: "/auth/error",
   },
   events: {
     async linkAccount({ user }) {
